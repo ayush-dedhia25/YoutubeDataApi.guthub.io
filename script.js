a83320e18b2d50dfcdd20e9cebb89bd220e0285c
ayush_dedhia25 <ayushsproductions@gmail.com>
2019-12-27 13:55:01 +0530

    Values

---
+++ script.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+	
+	$("#search").click(function() {
+		
+		var query = $("#query").val();
+		var API_KEY = "AIzaSyCUwT4UR5Wnhglu5GtpHxcARkK9Na9uePA";
+		var url = "https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&q="+query+"&key="+API_KEY;
+		var result = "";
+		
+		fetch(url)
+		.then(res => res.json())
+		.then(data => {
+			for (var i = 0; i < data.items.length; i++)
+			{
+				var id = data.items[i].id.videoId;
+				var title = data.items[i].snippet.title;
+				var description = data.items[i].snippet.description;
+				var thumbnail = data.items[i].snippet.thumbnails.default.url;
+				var channel_owner = data.items[i].snippet.channelTitle;
+				var link = "https://www.youtube.com/watch?v="+id;
+				
+				result = `
+					<div class="col-lg-4 col-md-4 col-sm-12 col-12 mb-5">
+		 				<div class="card" style="width: 18rem;">
+							<img class="card-img-top" src="${thumbnail}" alt="Card image cap">
+							<div class="card-body">
+			   					<h5 class="card-title">${title}</h5>
+			   					<p class="card-text">${description}</p>
+			   					<a href="${link}" class="btn btn-primary">View on Youtube</a>
+							</div>
+						</div>
+	  				</div>
+				`;
+				
+				$("#video_container").append(result);
+			}
+		})
+		.catch(err => alert("Error occured!"));
+		
+	});
+	
+});
